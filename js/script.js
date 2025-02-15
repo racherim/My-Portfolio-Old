05957426fd9170ebb481db8eb4156d7e7768ffcb
Aaron Neil Rodriguez <115781328+racherim@users.noreply.github.com>
2025-02-15 16:04:02 +0800

    added darkmode function

--- js/script.js
+++ js/script.js
@@ -54,7 +54,12 @@
     });
   }
 
-
-
+  //darkmode toggle
+  document.addEventListener('DOMContentLoaded', function() {
+      const toggle = document.getElementById('darkmode-toggle');
+      toggle.addEventListener('change', function() {
+          document.body.classList.toggle('dark-mode', toggle.checked);
+      });
+  });
 
 })(jQuery);
